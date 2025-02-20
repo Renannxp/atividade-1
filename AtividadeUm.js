d74d8c8d71ba4b0002370af1d738dfa6147c8a96
Renan Vinicius <renanvini1st@gmail.com>
2025-02-20 11:38:38 -0300

    Atividade 1

---
+++ AtividadeUm.js
@@ -0,0 +1,19 @@
+var nome = "Renan";
+var idade = "16";
+var atividade1 = "tocar";
+var atividade2 = "bateria";
+var atividade3 = "trompete";
+var curso = "T.I";
+console.log("".concat(nome, "!!"));
+console.log("Meu nome \u00E9 ".concat(nome, ", tenho ").concat(idade, " anos, no meu tempo\nlivre gosto de ").concat(atividade1, ", ").concat(atividade2, " e ").concat(atividade3, ". Tenho a inten\u00E7\u00E3o \nde me formar em ").concat(curso, ".\n"));
+/*
+1 - Na frase abaixo substitua _____ por uma variável que vai
+carregar a informação desejada
+
+
+BOA TARDE!!
+
+Meu nome é , tenho _____ anos, no meu tempo
+livre gosto de _______, ________ e ________. Tenho a intenção
+de me formar em _______.
+*/
---
+++ AtividadeUm.ts
@@ -0,0 +1,29 @@
+let nome = "Renan";
+let idade = "16";
+let atividade1 = "tocar";
+let atividade2 = "bateria";
+let atividade3 = "trompete";
+let curso = "T.I";
+
+
+console.log(`${nome}!!`)
+console.log (`Meu nome é ${nome}, tenho ${idade} anos, no meu tempo
+livre gosto de ${atividade1}, ${atividade2} e ${atividade3}. Tenho a intenção 
+de me formar em ${curso}.
+`)
+
+
+
+/*
+1 - Na frase abaixo substitua _____ por uma variável que vai 
+carregar a informação desejada
+
+
+BOA TARDE!!
+
+Meu nome é , tenho _____ anos, no meu tempo
+livre gosto de _______, ________ e ________. Tenho a intenção 
+de me formar em _______.
+*/
+
+
